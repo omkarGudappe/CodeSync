aad832089cb05c0e8651b48b3877ef83bdef82d1
omkarGudappe <150439784+omkarGudappe@users.noreply.github.com>
2025-07-13 10:41:15 +0530

    Update yjs-server.js

--- collaborative-code-editor/server/yjs-server.js
+++ collaborative-code-editor/server/yjs-server.js
@@ -10,7 +10,7 @@ wss.on('connection', (conn, req) => {
   setupWSConnection(conn, req);
 });
 
-const PORT = process.env.YjsPORT || 1234;
+const PORT = process.env.PORT || 1234;
 
 server.listen(PORT, '0.0.0.0', () => {
   console.log(`✅ Yjs WebSocket server running on ws://0.0.0.0:${PORT}`);
