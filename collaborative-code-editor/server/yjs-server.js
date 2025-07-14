0365f8674719c2d28e87e10826202674a6d4aec8
omkarGudappe <150439784+omkarGudappe@users.noreply.github.com>
2025-07-14 13:36:39 +0530

    Update yjs-server.js

--- collaborative-code-editor/server/yjs-server.js
+++ collaborative-code-editor/server/yjs-server.js
@@ -4,14 +4,23 @@ const { WebSocketServer } = WebSocket;
 const { setupWSConnection } = require('./utils.js');
 require('dotenv').config();
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+  if (req.url === '/health') {
+    res.writeHead(200);
+    res.end('OK');
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
+});
+
 const wss = new WebSocketServer({ server });
 
 wss.on('connection', (conn, req) => {
   setupWSConnection(conn, req);
 });
 
-const port = 1234; // use only this for Render!
-server.listen(port , () => {
+const port = process.env.PORT || 1234;
+server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
